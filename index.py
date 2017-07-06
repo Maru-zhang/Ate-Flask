c0ae724f9074d616e7e9b0bc4ba702c4416341a8
Maru <zhangbinhui@souche.com>
2017-07-07 01:30:15 +0800

    Support Draw Text in Pic

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from flask import Flask,render_template,request,make_response
 import time
 import hashlib
 import xml.etree.ElementTree as ET
+from PIL import Image,ImageDraw,ImageFont
 
 app = Flask(__name__)
 
@@ -26,3 +27,15 @@ def wechat_auth():
                 return make_response("认证失败")
         else:
             return "认证失败"
+
+
+@app.route("/test",methods = ["GET"])
+def test():
+    ttfont = ImageFont.truetype("/Library/Fonts/AppleGothic.ttf",20)
+    im = Image.open("image.png")  
+    draw = ImageDraw.Draw(im)  
+    draw.text((100,10),u'I am a girl.', fill=(0,0,0),font=ttfont)  
+    draw.text((40,40),unicode('hahahahha','utf-8'), fill=(0,0,0),font=ttfont)  
+    # im.show() 
+    im.save("./newImage.png")
+    return "success" 
--- manager.py
+++ manager.py
@@ -1,3 +1,4 @@
 from index import app
 app.debug = True
-app.run(host='0.0.0.0',port=80)
+# app.run(host='0.0.0.0',port=80)
+app.run(port=80)
